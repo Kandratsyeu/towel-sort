1cd4fdd6bd7284c8e4151a0aea6ade4f402c3315
Siarhei Kandratsyeu <sukandratsyeu@gmail.com>
2021-03-09 16:49:31 +0300

    task is completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix = []) {
+    return matrix
+        .map((el, index) => {
+            if (index % 2) {
+                let reversed = [];
+                el.forEach((item) => reversed.unshift(item));
+                return reversed;
+            } else {
+                return el;
+            }
+        })
+        .flat();
+};
